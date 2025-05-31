825438da7c1bc7e914f830e989e107f40d993b4b
Nicholas Lee <nicholas9lee@gmail.com>
2025-05-31 11:07:27 -0700

    added

---
+++ main.js
@@ -0,0 +1,5 @@
+import './style.css'
+
+// The HTML content is already defined in index.html, 
+// so we don't need to modify the DOM here anymore.
+// This file now only imports the necessary styles.
